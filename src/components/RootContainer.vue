abf1e5068a73785e2925e012924da2531a4c6c85
unknown <ajadvers@gmail.com>
2024-03-26 22:30:41 -0700

    first

---
+++ src/components/RootContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-200 h-screen w-[1024px] ml-auto mr-auto">
+    <NameHeader name="AJ Adversalo"/>
+  </div>
+</template>
+
+<script>
+import NameHeader from "./NameHeader.vue";
+
+export default {
+  name: 'RootContainer',
+  components: {    
+    NameHeader
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
